0d542ad3ead8a1543d3f06a1dab6a2f6ae62b423
limfiq <mtaufiq39@gmail.com>
2023-07-21 22:22:36 +0700

    Dashboard Created
    Menambahkan Halaman Dashboard

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -4,15 +4,18 @@
         <!-- Outer Row -->
         <div class="row justify-content-center">
 
-            <div class="col-xl-10 col-lg-12 col-md-9">
+            <div class="col-md-6">
 
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                            <div class="col-lg-6">
+                            <div class="col-md">
                                 <div class="p-5">
+                                    <div>
+                                        <img height =100 width=100 class=" img mx-auto img-rounded" align="center"src="<?=base_url('assets/');?>img/posyandu.jpg">
+                                    </div>
+                                    <br>
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">Selamat Datang</h1>
                                     </div>
